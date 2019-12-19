53204ba7c738a298158a0199842e747eb84e2317
Gabriela Voll <gabriela.voll@youvisit.com>
2019-12-18 23:37:37 -0500

    d3.js was the evil one, will need to research what its bumping into to make my life misery

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link, withRouter } from 'react-router';
 import { bindActionCreators } from 'redux';
-import * as d3 from "d3";
+//import * as d3 from "d3";
 
 const Home = () => (
 	<div>

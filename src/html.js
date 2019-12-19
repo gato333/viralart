c3659f7c7a76ee7f12e5943840254731376735aa
Gabriela Voll <gabriela.voll@youvisit.com>
2019-12-18 23:39:07 -0500

    removing bootstrap from html.js

--- src/html.js
+++ src/html.js
@@ -5,7 +5,6 @@ export default function renderHTMLwithInject(html, preloadedState) {
 	<head>
 	  <meta charset="UTF-8">
 	  <title>ViralArt - Home</title>
-	  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	  <link rel="stylesheet" href="/styles.css">
 	  <script src="https://d3js.org/d3.v5.min.js"></script>
 	</head>

ee8d5e7678ca51526bd83f358f34c2605578b8c2
Gabriela Voll <gabriela.voll@youvisit.com>
2019-12-30 03:32:05 -0500

    added route for next test, 2 dimensional mapping

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,7 @@ import Navigation from './design/Navigation.jsx';
 import DetailViewer from './DetailViewer.jsx';
 import GeometryTest from './GeometryTest.jsx';
 import FlatTest from './FlatTest.jsx';
+import TwoDTest from './TwoDTest.jsx';
 
 export default ({ match }) => (
 	<div>
@@ -11,6 +12,7 @@ export default ({ match }) => (
 		<DetailViewer />
 		
 		<Route path={`${match.path}/1`} component={GeometryTest} />
-		<Route path={[`${match.path}/2`, `${match.path}`]} component={FlatTest} />
+		<Route path={`${match.path}/2`} component={FlatTest} />
+		<Route path={[`${match.path}/3`, `${match.path}`]} component={TwoDTest} />
  	</div>
 );

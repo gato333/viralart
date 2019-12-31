f81d41c8f9de00b45d2cd9e2423454f8847b7508
Gabriela Voll <gabriela.voll@youvisit.com>
2019-12-30 20:58:59 -0500

    added gradient color to spectrum, and floor for depth

--- src/components/TwoDTest.jsx
+++ src/components/TwoDTest.jsx
@@ -9,7 +9,11 @@ import {
 	BoxGeometry,
 	MeshBasicMaterial,
 	MeshPhysicalMaterial,
-	SphereGeometry } from 'three';
+	SphereGeometry, 
+	PlaneGeometry,
+	DoubleSide,
+	Vector3,
+	Texture } from 'three';
 import { connect } from 'react-redux';
 
 const colorList = [ 0xff0000, 0xff7c00, 0xffff00, 0x7cff00, 0x00ff00, 0x00ff7c, 0x00ffff, 0x007cff, 0x0000ff, 0x7c00ff, 0xff00ff, 0xff007c ];
@@ -18,6 +22,7 @@ var scene, camera, renderer, light, boxes = [], spheres = [];
 class TwoDTest extends React.Component { 
 	constructor(props){
 		super(props);
+		this.generateTexture = this.generateTexture.bind(this);
 	}
 
 	componentDidMount(){
@@ -32,11 +37,29 @@ class TwoDTest extends React.Component {
 		light.position.set( -2, 10, 20 );
 		light.castShadow = true;
 		scene.add( light );
+		// floor
+		var floorGeo = new PlaneGeometry(300, 1000);
+		var floorMaterial = new MeshPhysicalMaterial({ color: 0xffffff, flatShading: true });
+		var floor = new Mesh( floorGeo, floorMaterial );
+		floor.receiveShadow = true; 
+		floor.castShadow = true;
+		floor.position.set(0,-50,-200);
+		floor.rotation.set(-3* Math.PI / 7, 0,0);
+		scene.add(floor);
+		
 		//Color Spectrum
 		for( var i = 0; i < colorList.length; i++){
 			var boxGeo = new BoxGeometry( 15, 100, 0 );	
-			var boxMaterial =  new MeshBasicMaterial( { color: colorList[i] } );
+
+			// material texture
+			var texture = new Texture( this.generateTexture(colorList[i]) );
+			texture.needsUpdate = true; // important!
+			// material
+			var boxMaterial = new MeshBasicMaterial({ map: texture });
+
 			var box = new Mesh( boxGeo, boxMaterial );
+			box.receiveShadow = true;
+			box.castShadow = true;
 			box.position.set( -85 + (15 * i) , 0, -120);
 			boxes.push(box);
 			scene.add( box );
@@ -49,9 +72,8 @@ class TwoDTest extends React.Component {
 			var r = parseInt(color.slice(0, 2), 16),
 				g = parseInt(color.slice(2, 4), 16),
 				b = parseInt(color.slice(4, 6), 16);
-			var geometry = new SphereGeometry( 5, 32, 16 );
-			//geometry.computeFlatVertexNormals();
-			var material =   new MeshPhysicalMaterial( {
+			var geometry = new SphereGeometry( 3, 32, 16 );
+			var material = new MeshPhysicalMaterial( {
 				color: parseInt(color, 16),
 				roughness: 0.7,
 				transparency: 0.15,
@@ -69,14 +91,42 @@ class TwoDTest extends React.Component {
 			if(max == r) xVal = (g/255) * 60 - 85;
 			else if(max == g) xVal = ((b/255) * 60) + 60 - 85;
 			var yVal = -50; 
-			var collisions = spheres.filter( x => x.position.x >= xVal - 10 && x.position.x <= xVal + 10 );
-			if( collisions.length > 0 ) yVal = Math.max( ...collisions.map( x => x.position.y )) + 10;
-			sphere.position.set(xVal, yVal, -120)
+			var collisions = spheres.filter( x => x.position.x >= xVal - 5 && x.position.x <= xVal + 5 );
+			if( collisions.length > 0 ) yVal = Math.max( ...collisions.map( x => x.position.y )) + 6;
+			sphere.position.set(xVal, yVal, -117);
+			sphere.castShadow = true;
 			spheres.push(sphere);
 			scene.add( sphere );
 		}
-
+	
 		renderer.render( scene, camera );
+	}
+
+	generateTexture(hex) {
+		var width = 15;
+		var height = 100;
+		hex = hex.toString(16);
+		var color = "#" + ("000000".substr(0, 6 - hex.length) + hex);
+
+		// create canvas
+		var canvas = document.createElement( 'canvas' );
+		canvas.width = width;
+		canvas.height = height;
+
+		// get context
+		var context = canvas.getContext( '2d' );
+
+		// draw gradient
+		context.rect( 0, 0, width, height );
+		var gradient = context.createLinearGradient( 0, 0, width, height );
+		gradient.addColorStop(0, '#ffffff'); // white
+		gradient.addColorStop(.3, color);
+		gradient.addColorStop(.7, color);
+		gradient.addColorStop(1, '#000000'); // black
+		context.fillStyle = gradient;
+		context.fill();
+
+		return canvas;
 	}
 
 	onClick(){
